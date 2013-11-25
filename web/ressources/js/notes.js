dba691d29d7a4a895385ea9bda671a2b66527600
Natuu <natyouu@gmail.com>
2013-11-25 22:09:30 +0100

    Début de javascript pour les notes

--- web/ressources/js/notes.js
+++ web/ressources/js/notes.js
@@ -1,5 +1,19 @@
 $(function() {
 
+	// Cette fonction redimmensionner les boites des moyennes
+	for (var i = $('.graph-hidden>td>div').get().length - 1; i >= 0; i--)
+	{
+		var reg = new RegExp("(jauge-)", "g");
+
+		var matiere = $('.graph-hidden>td>div').get(i).className.replace(reg, "") ;
+		var moyenneMatiere = $('.moyenne-' + matiere).text();
+		var tailleJauge = moyenneMatiere * 2 + 'px' ;
+
+		$('.jauge-' + matiere).class('height', tailleJauge);
+
+		console.log();
+	}
+
 	// Cette fonction lance l'animation des notes au lancement
 
 	$('.graph-hidden')
