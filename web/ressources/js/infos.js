6ae3de7faf49fe8061223d6f229ff529e948f30b
Natuu <natyouu@gmail.com>
2013-11-27 23:10:37 +0100

    Gestion de l'affichage d'un eleve (tout roles compris)

--- web/ressources/js/infos.js
+++ web/ressources/js/infos.js
@@ -2,4 +2,9 @@ $(function() {
 
 	// Cette fonction retire le second titre
 
+	$('.section-title:contains("Mes notes")').unwrap().html('');
+	$('.notes-by-matieres').unwrap().html('');
+	$('.wrap-details').unwrap();
+
+
 });
