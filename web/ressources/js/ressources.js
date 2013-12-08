edd5473261655c0ff6d9503b0b90105ad6bea472
Natuu <natyouu@gmail.com>
2013-12-08 05:23:20 +0100

    Fin de la mise en page des ressources

--- web/ressources/js/ressources.js
+++ web/ressources/js/ressources.js
@@ -1,5 +1,7 @@
 $(function() {
 
+	var toShow = new Array;
+
 	$('.options-tri').hide();
 
 	$('.trier').click( function() {
@@ -9,19 +11,60 @@ $(function() {
 	})
 
 	$('.niveau>li').click( function() {
+
 		$(this).toggleClass('unchecked');
 
-		var niveau = $(this).attr('class');
+		var niveau = $(this).attr('id');
 
-		$('.niveau-' + niveau).toggle();
+		if (!$(this).hasClass('unchecked'))
+		{
+			toShow.push(niveau);
+			$('.' + niveau).show();
+		}
+		else
+		{
+			$('.' + niveau).hide();
+
+			for (var i = toShow.length - 1; i >= 0; i--) {
+				if (toShow[i] == niveau)
+				{
+					delete toShow[i];
+				}
+
+				else if ($('.' + niveau).hasClass(toShow[i]))
+				{
+					$('.' + niveau).show();
+				}
+			}
+		}
 	})
 
 	$('.type>li').click( function() {
+
 		$(this).toggleClass('unchecked');
 
-		var type = $(this).attr('class');
+		var type = $(this).attr('id');
 
-		$('.type-' + niveau).toggle();
+		if (!$(this).hasClass('unchecked'))
+		{
+			toShow.push(type);
+			$('.' + type).show();
+		}
+		else
+		{
+			$('.' + type).hide();
+			
+			for (var i = toShow.length - 1; i >= 0; i--) {
+				if (toShow[i] == type)
+				{
+					delete toShow[i];
+				}
+				else if ($('.' + type).hasClass(toShow[i]))
+				{
+					$('.' + type).show();
+				}
+			}
+		}
 	})
 
 	$('#matiere')
@@ -29,10 +72,25 @@ $(function() {
 	    placeholder: "Choisir une matière",
 	    allowClear: true
 	})
-	.on("change", function(e) {
-		for (var i = e.val.length - 1; i >= 0; i--) {
-		 	$('.matiere-' + e.val[i]).toggle();
-		}; 
+	.on("select2-selecting", function(e) {
 
+		toShow.push(e.val);
+	 	$('.' + e.val).show();
+	 	console.log(e.val);
+	})
+	.on("select2-removing", function(e) {
+	 	
+	 	$('.' + e.val).hide();
+
+	 	for (var i = toShow.length - 1; i >= 0; i--) {
+			if (toShow[i] == e.val)
+			{
+				delete toShow[i];
+			}
+			else if ($('.' + e.val).hasClass(toShow[i]))
+			{
+				$('.' + type).show();
+			}
+		}
 	});
 });
