242a20ec82c4f60b1131553ec6ae5fe33f026b8d
Natuu <natyouu@gmail.com>
2013-12-14 10:24:01 +0100

    Mise en forme de quelques formulaires

--- web/ressources/js/appel.js
+++ web/ressources/js/appel.js
@@ -1,10 +1,25 @@
-$(function() {
-
-	$('.input-appel').hide();
+$(function() {	
 
 	// Cette fonction permet de changer la couleur et la presence d'un eleve au clic
 
 	var inputAppel = $('.input-appel');
+	
+	inputAppel.hide();
+	
+	for (var i = inputAppel.length - 1; i >= 0; i--) {
+
+		var this = $('.input-appel');
+
+		console.log(this);
+		// if (this.val() == '0')
+		// {
+			
+		// }
+		// else
+		// {
+
+		// }
+	};
 
 	$('.in-list')
 	.click( function() {
