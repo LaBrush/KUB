14ca0ec7c08fefec636dac2725aa6b4891fa2908
Natuu <natyouu@gmail.com>
2013-12-21 14:00:38 +0100

    Modification des ressources

--- web/ressources/js/ressources.js
+++ web/ressources/js/ressources.js
@@ -1,85 +1,42 @@
 $(function() {
 
-	// Fonctions de l'appercu
-
-	$('.ressource-selectionnee').hide();
-
-	$('.in-list').click( function() {
-		var index = $('.in-list').index(this);
-
-		$('.preview').attr("src", "about:blank");
-
-		$('.ressource-selectionnee').hide();
-
-		$('html, body').animate({scrollTop:0}, 'slow');
-
-		$('.ressource-selectionnee').eq(index).show();
-
-		$('.url').hide();
-
-		$('.preview').attr("src", $('.url').eq(index).text());
-	})
-
 	// Fonctions de tri
 
-	var toShow = new Array;
+	var toHide = new Array;
 
-	$('.niveau>li').click( function() {
+	$('.options-tri>li').click( function() {
 
 		$(this).toggleClass('unchecked');
 		$(this).toggleClass('checked');
 
 
-		var niveau = $(this).attr('id');
+		var option = $(this).attr('id');
 
-		if (!$(this).hasClass('unchecked'))
+
+		if ($(this).hasClass('unchecked'))
 		{
-			toShow.push(niveau);
-			$('.' + niveau).show();
+			toHide.push(option);
+
+			$('.ressource').show();
 		}
 		else
 		{
-			$('.' + niveau).hide();
-
-			for (var i = toShow.length - 1; i >= 0; i--) {
-				if (toShow[i] == niveau)
+			for (var i = toHide.length - 1; i >= 0; i--) {		
+				if (toHide[i] == option)
 				{
-					delete toShow[i];
-				}
-
-				else if ($('.' + niveau).hasClass(toShow[i]))
-				{
-					$('.' + niveau).show();
+					delete toHide[i];
 				}
 			}
+
+			$('.' + option).show();
 		}
-	})
 
-	$('.type>li').click( function() {
-
-		$(this).toggleClass('unchecked');
-		$(this).toggleClass('checked');
-
-
-		var type = $(this).attr('id');
-
-		if (!$(this).hasClass('unchecked'))
-		{
-			toShow.push(type);
-			$('.' + type).show();
-		}
-		else
-		{
-			$('.' + type).hide();
+		for (var i = toHide.length - 1; i >= 0; i--) {
 			
-			for (var i = toShow.length - 1; i >= 0; i--) {
-				if (toShow[i] == type)
+			for (var j = $('.ressource').length - 1; j >= 0; j--) {
+				if ($('.ressource').eq(j).hasClass(toHide[i]))
 				{
-					delete toShow[i];
-				}
-				else if ($('.' + type).hasClass(toShow[i]))
-				{
-					$('.' + type).show();
+					$('.ressource').eq(j).hide();
 				}
 			}
 		}
@@ -92,21 +49,41 @@ $(function() {
 	})
 	.on("select2-selecting", function(e) {
 
-		toShow.push(e.val);
-	 	$('.' + e.val).show();
+		if ($(this).val() == null)
+		{
+			$('.ressource').addClass('ressource-hidden').removeClass('ressource');
+		}
+
+	 	$('.' + e.val).addClass('ressource').removeClass('ressource-hidden').removeAttr('style');
+
+	 	for (var i = toHide.length - 1; i >= 0; i--) {
+			
+			for (var j = $('.ressource').length - 1; j >= 0; j--) {
+				if ($('.ressource').eq(j).hasClass(toHide[i]))
+				{
+					$('.ressource').eq(j).hide();
+				}
+			}
+		}
 	})
 	.on("select2-removing", function(e) {
-	 	
-	 	$('.' + e.val).hide();
 
-	 	for (var i = toShow.length - 1; i >= 0; i--) {
-			if (toShow[i] == e.val)
-			{
-				delete toShow[i];
-			}
-			else if ($('.' + e.val).hasClass(toShow[i]))
-			{
-				$('.' + type).show();
+		if ($(this).val() == null)
+		{
+			$('.ressource-hidden').addClass('ressource').removeClass('ressource-hidden');
+		} 
+		else
+		{
+			$('.' + e.val).addClass('ressource-hidden').removeClass('ressource');
+		}
+
+		for (var i = toHide.length - 1; i >= 0; i--) {
+			
+			for (var j = $('.ressource').length - 1; j >= 0; j--) {
+				if ($('.ressource').eq(j).hasClass(toHide[i]))
+				{
+					$('.ressource').eq(j).hide();
+				}
 			}
 		}
 	});
