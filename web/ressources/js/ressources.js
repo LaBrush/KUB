7781901c94c0ddc6fbf07ae23e16f998408d1633
Natuu <natyouu@gmail.com>
2014-01-26 17:17:42 +0100

    Toujours plus loin

--- web/ressources/js/ressources.js
+++ web/ressources/js/ressources.js
@@ -96,3 +96,28 @@ $(function() {
 		};
 	}
 });
+
+
+
+
+
+// Pour le formulaire
+$('.file').hide();
+$('input:file').removeAttr('required');
+
+$('input:radio').click(function() {
+	if ($('input:radio')[0].checked)
+	{
+		$('.file').hide();
+		$('input:file').removeAttr('required');
+		$('.url').show();
+		$('input:url').attr('required', 'required');
+	}
+	else
+	{
+		$('.file').show();
+		$('input:file').attr('required', 'required');
+		$('.url').hide();
+		$('input:url').removeAttr('required');
+	}
+})
