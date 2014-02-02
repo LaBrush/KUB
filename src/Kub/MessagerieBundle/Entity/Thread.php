df6a3167526c35f32c1c19aef32ef56c3349993f
alphindia <adrienbocquet38gmail.com>
2014-02-02 18:39:20 +0100

    ordre des messages

--- src/Kub/MessagerieBundle/Entity/Thread.php
+++ src/Kub/MessagerieBundle/Entity/Thread.php
@@ -23,6 +23,7 @@ class Thread
 
     /**
      * @ORM\OneToMany(targetEntity="Kub\MessagerieBundle\Entity\Message", mappedBy="thread", cascade={"persist", "merge", "detach"})
+     * @OrderBy({"date" = "DESC"})
      */
     private $messages ;
 
