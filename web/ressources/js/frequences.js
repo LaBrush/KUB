7693553d2c4334e7a57acc4b74e0173d0781e060
Natuu <natyouu@gmail.com>
2014-02-12 13:08:37 +0100

    Page des frequences faite

---
+++ web/ressources/js/frequences.js
@@ -0,0 +1,22 @@
+$(function() {	
+
+	$('input:checkbox').hide();
+
+	$('td').click(function() {
+		var $this = $(this);
+		$this.toggleClass('checked').find('input:checkbox').prop('checked', $this.hasClass('checked'));
+	});
+
+	if ($('input:checkbox').prop('checked'))
+	{
+		$(this).parent().addClass('checked');
+	}
+
+	$('.semaine1').appendTo($('.line1'));
+
+	$('.semaine2').appendTo($('.line2'));
+
+	$('.semaine3').appendTo($('.line3'));
+
+	$('.semaine4').appendTo($('.line4'));
+});
